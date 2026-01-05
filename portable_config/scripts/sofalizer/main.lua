38639f18e65016dbcdaf8bb0287ccbc01aa00347
PopeyeURS <118032753+PopeyeURS@users.noreply.github.com>
2026-01-05 17:00:07 +0800

    Update main.lua

--- portable_config/scripts/sofalizer/main.lua
+++ portable_config/scripts/sofalizer/main.lua
@@ -4,3 +4,9 @@ local mp = require 'mp'
 -- Load your modular audio scripts
 dofile(mp.find_config_file("scripts/main_cinema.lua"))
 dofile(mp.find_config_file("scripts/main_headset.lua"))
+
+-- Startup confirmation
+mp.register_event("start-file", function()
+    mp.osd_message("🎶 IMAX audio bindings loaded (F9/F10/F11)", 4)
+    mp.msg.info("Sofalizer main.lua: Cinema + Headset modules initialized")
+end)
