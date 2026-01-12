4e5fc1689f44ceb0517b62ccc9a90842562809c6
PopeyeURS <ulyssesrscaballes@icloud.com>
2026-01-12 09:19:30 +0800

    Remove corrupted portable_config

--- portable_config/shaders/Color_tweaks/microcontrast.glsl
+++
@@ -1,8 +0,0 @@
-//!HOOK MAIN
-//!BIND MAIN
-//!DESC Microcontrast Boost
-vec4 hook() {
-    vec4 color = texel(MAIN, pos);
-    color.rgb = pow(color.rgb, vec3(1.05)); // Slight gamma lift
-    return color;
-}
