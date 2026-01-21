4a8ee62ad48c929922dae8260f4ab374ee691864
PopeyeURS <118032753+PopeyeURS@users.noreply.github.com>
2026-01-21 21:31:38 +0800

    Refine audio settings in set_music_audio function
    Updated audio processing parameters for improved sound quality. Delicately fine tuned to feel the full harmonic orchestra: from the rumble of the basses to the sparkle of the cymbals, every instrument now has its rightful place. ENJOY!!!

--- portable_config/scripts/sofalizer/main.lua
+++ portable_config/scripts/sofalizer/main.lua
@@ -53,26 +53,38 @@ local function set_music_audio()
     mp.commandv("af", "set",
         "lavfi=[aid]sofalizer=sofa='" .. SOFA_PATH .. "':gain=1.0:type=hrtf:normalize=1:interpolate=1," ..
         "loudnorm=I=-23:TP=-2:LRA=8," ..
-        "acompressor=threshold=-20dB:ratio=2.0:attack=8:release=110," ..
-        "equalizer=f=50:t=q:w=1.2:g=2," ..
+        "acompressor=threshold=-15dB:ratio=2.4:attack=8:release=95," ..
+        "equalizer=f=50:t=q:w=1.2:g=2.0," ..
         "equalizer=f=65:t=q:w=1.2:g=1.2," ..
-        "equalizer=f=80:t=q:w=1.0:g=0.8," ..
-        "equalizer=f=95:t=q:w=1.0:g=1.0," .. 
+        "equalizer=f=80:t=q:w=1.0:g=2.5," ..
+        "equalizer=f=95:t=q:w=1.0:g=1.0," ..
+        "equalizer=f=100:t=q:w=1.0:g=2.0," ..
         "equalizer=f=120:t=q:w=1.0:g=-0.5," ..
         "equalizer=f=145:t=q:w=1.0:g=0.7," ..
-        "equalizer=f=500:t=q:w=1.0:g=0.5," ..
+        "equalizer=f=200:t=q:w=1.0:g=1.8," ..
         "equalizer=f=250:t=q:w=1:g=-1.5," ..
-        "equalizer=f=1000:t=q:w=0.8:g=2," ..
+        "equalizer=f=350:t=q:w=1.0:g=1.2," ..
+        "equalizer=f=400:t=q:w=1.0:g=1.5," ..
+        "equalizer=f=500:t=q:w=1.0:g=0.5," ..
+        "equalizer=f=600:t=q:w=1.0:g=1.2," ..
+        "equalizer=f=1000:t=q:w=0.8:g=2.0," ..
+        "equalizer=f=1800:t=q:w=0.8:g=1.5," ..
         "equalizer=f=2000:t=q:w=0.8:g=0.9," ..
+        "equalizer=f=2500:t=q:w=0.9:g=1.5," ..
         "equalizer=f=3000:t=q:w=0.8:g=2.5," ..
+        "equalizer=f=3500:t=q:w=0.8:g=2.0," ..
         "equalizer=f=4000:t=q:w=0.8:g=0.7," ..
         "equalizer=f=5000:t=q:w=0.8:g=2.5," ..
+        "equalizer=f=7000:t=q:w=0.8:g=2.5," ..
         "equalizer=f=8000:t=q:w=0.7:g=1.9," ..
-        "equalizer=f=12000:t=q:w=0.7:g=2.4," ..
-        "equalizer=f=16000:t=q:w=0.7:g=1.9," ..
+        "equalizer=f=9000:t=q:w=0.7:g=2.0," ..
+        "equalizer=f=10000:t=q:w=0.7:g=3.0," ..
+        "equalizer=f=12000:t=q:w=0.7:g=3.5," ..
+        "equalizer=f=14000:t=q:w=0.7:g=3.0," ..
+        "equalizer=f=16000:t=q:w=0.7:g=2.5," ..
         "stereotools=mlev=1.08:slev=1.08:phase=0.85:width=1.43," ..
         "aecho=0.8:0.9:30:0.25," ..
-        "alimiter=level_in=1:level_out=0.985:limit=0.09," ..
+        "alimiter=level_in=1:level_out=0.99:limit=0.16," ..
         "aresample=48000]"
     )
     show("🎵 Music Mode Plus", 4)
