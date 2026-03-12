0b2c0017ae92fa288203f6d70f89a8ca9116c2f1
PopeyeURS <118032753+PopeyeURS@users.noreply.github.com>
2026-03-12 13:40:17 +0800

    Update file paths to use generic user directory

--- portable_config/scripts/sofalizer/main.lua
+++ portable_config/scripts/sofalizer/main.lua
@@ -5,8 +5,8 @@ local utils = require "mp.utils"
 
 -- CONFIG
 
-local KEMAR_SOFA = "C:/Users/ulyss/AppData/Roaming/mpv/portable_config/scripts/sofalizer/KEMAR_HRTF.sofa"
-local SADIE_BRIR = "C:/Users/ulyss/AppData/Roaming/mpv/portable_config/scripts/sofalizer/SADIE_KEMAR_DFC_256_order_fir_48000.sofa"
+local KEMAR_SOFA = "C:/Users/user_name/AppData/Roaming/mpv/portable_config/scripts/sofalizer/KEMAR_HRTF.sofa"
+local SADIE_BRIR = "C:/Users/user_name/AppData/Roaming/mpv/portable_config/scripts/sofalizer/SADIE_KEMAR_DFC_256_order_fir_48000.sofa"
 
 -- HELPERS
 
